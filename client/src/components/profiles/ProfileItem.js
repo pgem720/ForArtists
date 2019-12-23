40c5d5a02a3b9037eb688e4e697ed269063498a1
Pia Coronel <piacoronel@gmail.com>
2019-12-23 12:30:53 -0500

    fixed website link

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -15,7 +15,7 @@ const ProfileItem = ({
 			<img src={avatar} alt='' className='round-img' />
 			<div>
 				<h2>{name}</h2>
-				<a href={website}>{website}</a>
+				<a href={website}>website</a>
 				<p className='my-1'>{location && <span>{location}</span>}</p>
 				<Link to={`/profile/${_id}`} className='btn btn-primary'>
 					View Profile
